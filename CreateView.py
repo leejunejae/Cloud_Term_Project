5fbd42dc57da902fd0ce00f541ee5bea7edb74d5
leejunjae <46302891@naver.com>
2022-12-05 20:40:49 +0900

    2022-12-05 Add Instance control code

---
+++ CreateView.py
@@ -0,0 +1,15 @@
+import boto3, os
+from dotenv import load_dotenv
+from boto3.session import Session
+
+
+#6
+
+load_dotenv()
+
+ec2 = boto3.resource('ec2', aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+                   aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"), )
+
+image_id = input()
+
+ec2.create_instances(ImageId=image_id, MinCount=1, MaxCount=1, InstanceType='t2.micro', )
